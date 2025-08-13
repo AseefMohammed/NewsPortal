2c5348ba4a5e13cf7a65c9f1ccfec7e42d637196
Aseef Mohammed <asif.elegance@gmail.com>
2025-08-13 19:06:21 +0400

    Remove exports for deleted components from modernIndex.tsx

--- web/src/modernIndex.tsx
+++ web/src/modernIndex.tsx
@@ -1,3 +1,1 @@
-export { default as GlowButton } from './GlowButton';
-export { default as AnimatedCard } from './AnimatedCard';
-export { default as GradientBackground } from './GradientBackground';
+export {};
