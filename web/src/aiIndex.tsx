cfa97f8baa99a57cde7fb11c27656f63b47087bd
Aseef Mohammed <asif.elegance@gmail.com>
2025-08-13 19:03:29 +0400

    Update aiIndex.tsx to remove exports for deleted components

--- web/src/aiIndex.tsx
+++ web/src/aiIndex.tsx
@@ -1,4 +1,1 @@
-
-export { default as AISummaryComponent } from './AISummaryComponent';
 export { default as SmartSearchComponent } from './SmartSearchComponent';
-export { default as PersonalizedFeedComponent } from './PersonalizedFeedComponent';
